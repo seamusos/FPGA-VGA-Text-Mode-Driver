5ac6c2b279c9510efba51fe102f9e79a9796c65f
Seamus <seamus.osullivan@outlook.com.au>
2020-10-19 16:01:13 +1100

    Did i break something?

--- Quartus/framebuffer.v
+++ Quartus/framebuffer.v
@@ -2,7 +2,7 @@
 
 module framebuffer(clk, counterX, counterY, spi_shift_reg, spi_done, pixel_out);
 
-input clk;
+input clk;	//Core Clock 50Mhz
 input wire [9:0] counterX;
 input wire [9:0] counterY;
 input wire [2:0] spi_done;
@@ -12,20 +12,20 @@ output wire pixel_out;
 
 
 
-wire [6:0] column;
-wire [4:0] row;
-wire [11:0] ascii_address;
-wire [7:0] ascii_value;
+wire [6:0] column;			//
+wire [4:0] row;				//Row of pixel in 8x16 glyph matrix
+wire [11:0] ascii_address;	
+wire [7:0] ascii_value;		//
 
-wire [13:0] glyph_address;
+wire [13:0] glyph_address;	//Address of pixel for corresponding ASCII code
 
 
 reg [2:0] glyph_pos_x;	//8
 reg [3:0] glyph_pos_y;	//16
 
-reg [2399 : 0] write_address;
-reg [7:0] data;
-reg write_en;
+reg [2399 : 0] write_address;	//Address to be written to
+reg [7:0] data;	//data port from SPI shift reg
+reg write_en;		//Enables RAM Write
 
 
 assign column = counterX[9:3];
@@ -38,7 +38,7 @@ assign ascii_address = column + (row * 80);
 //text_ram text_mem(.address(ascii_address), .clock(clk), .q(ascii_value));
 
 
-
+// Two port RAM Block to store ASCII reference from SPI
 twoport textRam(.clock(clk), .data(data), .rdaddress(ascii_address), .wraddress(write_address), .wren(write_en), .q(ascii_value));
 
 
@@ -48,23 +48,22 @@ always @(posedge clk) begin
 		data = spi_shift_reg;
 		write_address = write_address + 1; //Increment to next char
 	end else
-		write_en <= 1'b0;
+		write_en = 1'b0;
 end
 
 
 
 always @(posedge clk) begin
-	glyph_pos_x <= counterX[2:0];
+	glyph_pos_x <= counterX[2:0];	// Every
 	glyph_pos_y <= counterY[3:0];
 end
 
-//Calculates Position in ROM
+//Calculates Address Position in ROM from ASCII Code, then returns bit based on pixel position
 assign glyph_address = (ascii_value << 7) + glyph_pos_x + (glyph_pos_y << 3);
 
 //Glyph memory
-
 glyph_rom glyph(.address(glyph_address),.clock(clk), .q(pixel_out));
 
 
 
-endmodule
+endmodule 
--- Quartus/vga.v
+++ Quartus/vga.v
@@ -7,44 +7,49 @@ input sck; //Pin_32
 input mosi;	//Pin_31
 input cs;	//Pin_30
 
-
+/* VGA Pin Parameters */
 output vga_h_sync; //Pin 101
 output vga_v_sync; //Pin 103
 output reg vga_r; //Pin 106
 output reg vga_g; //Pin 105
 output reg vga_b; //Pin 104
 
-output stupid_buzzer;
 
+// buzzer is going off, idk why
+output stupid_buzzer;
 assign stupid_buzzer = 1'b1;
 
 
 
-wire pixel_clock;	
-wire inDisplayArea;
-wire [9:0] CounterX;
-wire [9:0] CounterY;
-wire [3:0] selector;
+wire pixel_clock;		//~25Mhz Clock for VGA Pulse Signals
+wire inDisplayArea;	// If pixel is within display limits
+wire [9:0] CounterX;	// Tracks horizontal pulse
+wire [9:0] CounterY;	// Tracks Verticle pulse
 
-wire [2:0] pixel_out;
+//wire [3:0] selector;
 
-wire [7:0] ascii_shift_reg;
+wire [2:0] pixel_out;	//Output from Fram Buffer
+
+wire [7:0] ascii_shift_reg;	//Output from SPI driver
 
 wire spi_done;
-wire [2:0] spi_rising;
+wire [2:0] spi_rising;	// Rising CS signal for end of transaction
 
+// Divides core clock for pixel Clock
 frac_clock_divider clk_divider(.clk(clk), .clk_div(pixel_clock));
 
+// Generates Horizontal and Verticle sync pulses
 sync_generator hvsync(.clk(pixel_clock), .vga_h_sync(vga_h_sync), .vga_v_sync(vga_v_sync), .inDisplayArea(inDisplayArea), .CounterX(CounterX), .CounterY(CounterY));
 
+// 8-Bit SPI Slave driver
 SPI_Module SPI(.clk(clk), .sck(sck), .mosi(mosi), .cs(cs), .input_shiftreg(ascii_shift_reg), .done(spi_done), .cs_record(spi_rising));
 
-
+// Generates Image from memory for VGA
 framebuffer buffer(.clk(clk), .counterX(CounterX), .counterY(CounterY), .spi_shift_reg(ascii_shift_reg), .spi_done(spi_rising), .pixel_out(pixel_out));
 
 always @(posedge pixel_clock)
 begin
-	if (inDisplayArea) begin
+	if (inDisplayArea) begin	// If within display Region
 		vga_r <= pixel_out;
 		vga_g <= pixel_out;
 		vga_b	<= pixel_out;
